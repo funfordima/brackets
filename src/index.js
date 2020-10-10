1dbf4a8dd2587ccfd5653a200ed98c841b5515d5
funfordima <d.g.litvinov@gmail.com>
2020-10-10 19:04:41 +0300

    feat: implement task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let result = str;
+
+    for (let i = 0; i < str.length / 2; i++) {
+        bracketsConfig.forEach((arr) => {
+            const bracketsString = arr.join('');
+
+            while (result.includes(bracketsString)) {
+                result = result.replace(bracketsString, '');
+            }
+        });
+    }
+
+    if (result.length !== 0 || result == str) {
+        return false;
+    } else {
+        return true;
+    }
+};
